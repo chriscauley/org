d7005a319f701d54a7fe63be8a455aef04d75547
chriscauley <chris@lablackey.com>
2014-03-12 19:42:28 +0000

    adding hush login and git.lg

--- ln.sh
+++ ln.sh
@@ -2,3 +2,7 @@ cd ~
 ln -s org/.bash_aliases .
 ln -s org/.screenrc .
 ln -s org/.emacs .
+
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit"
+
+touch ~/.hushlogin
