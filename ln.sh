bba783e61ae947be21d5f07ba8065b43a0e83ec3
chriscauley <chris@lablackey.com>
2014-08-09 15:49:21 +0000

    adding git config to ln.sh

--- ln.sh
+++ ln.sh
@@ -6,3 +6,6 @@ ln -s org/.emacs .
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit"
 
 touch ~/.hushlogin
+
+git config --global user.email "chris@lablackey.com"
+git config --global user.name "chriscauley"
