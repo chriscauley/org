68a0529ca794642fd7983a34efc55b69ee17393b
chriscauley <chris@lablackey.com>
2015-06-03 13:41:13 -0400

    start.sh

---
+++ start.sh
@@ -0,0 +1,3 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install emacs24-nox git screen mlocate python-dev nodejs npm nginx uwsgi uwsgi-plugin-python postgresql-server-dev-9.3 postgresql
