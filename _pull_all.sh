5dc38732b9099dfbca98b805ec344f80ce9c3f12
chriscauley <chris@lablackey.com>
2017-09-02 20:26:26 +0000

    adding has to pull all command

--- _pull_all.sh
+++ _pull_all.sh
@@ -3,9 +3,14 @@ do
     if [ -f $DIR/.git/config ]
     then
         if grep "bare...false" $DIR/.git/config > /dev/null
-           then
-               echo pulling $DIR
-               cd $DIR; git pull & cd ..
+        then
+            if [[ $1 = "hash" ]]
+            then
+                echo $DIR @ `cd $DIR;git rev-parse HEAD;cd ..`
+            else
+                echo pulling $DIR
+                cd $DIR; git pull & cd ..
+            fi
         fi
     fi
 done
