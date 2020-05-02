068ea6b9cb30e9dbf93bedf0c586ee308c8de333
chriscauley <chris@lablackey.com>
2020-05-02 18:12:59 +0000

    global gitignore

--- ln.sh
+++ ln.sh
@@ -3,11 +3,7 @@ ln -s org/.bash_aliases .
 ln -s org/.screenrc .
 ln -s org/.emacs .
 ln -s org/.tmux.conf .
-if [ ! -f .dev/_gulp.sh ]
-then
-    mkdir ~/.dev/
-    ln -s org/_gulp.sh .dev/
-fi
+ln -s org/.gitignore_global .
 
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit"
 
@@ -15,3 +11,4 @@ touch ~/.hushlogin
 
 git config --global user.email "chris@lablackey.com"
 git config --global user.name "chriscauley"
+git config --global core.excludesfile ~/.gitignore_global
